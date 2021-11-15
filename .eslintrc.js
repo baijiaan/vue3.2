8130599ea5bf11c4fdc55c3ae0a735a2f68c77a2
Bja-ja <b17611596313@163.com>
2021-11-15 17:23:54 +0800

    feat(代码调试): 新增代码的配置文件,使用git注释提交规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+   'quotes':'off',
+   'comma-dangle':1,
+   'space-before-function-paren':0
   }
 }
